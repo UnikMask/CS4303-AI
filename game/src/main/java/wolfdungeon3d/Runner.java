17ac4c0b46a87e766af4a81baab7052ad9840818
UnikMask <alexandre.guerville@hotmail.com>
2023-02-26 17:38:06 +0000

    Add PApplet inheritance

--- game/src/main/java/wolfdungeon3d/Runner.java
+++ game/src/main/java/wolfdungeon3d/Runner.java
@@ -3,12 +3,34 @@
  */
 package wolfdungeon3d;
 
-public class Runner {
-	public String getGreeting() {
-		return "Hello World!";
+import processing.core.PApplet;
+import processing.core.PGraphics;
+import processing.core.PVector;
+
+public class Runner extends PApplet {
+	static private final PVector MAIN_CTX_PERCENT = new PVector(1f, 0.8f);
+	static private final PVector MAIN_CTX_POS = new PVector();
+
+	PGraphics mainGraphicsCtx;
+
+	public void setup() {
+		frameRate(60);
+		mainGraphicsCtx = createGraphics((int) ((float) width * MAIN_CTX_PERCENT.x),
+				(int) ((float) height * MAIN_CTX_PERCENT.y), PApplet.P2D);
+	}
+
+	public void settings() {
+		size(1920, 1080, PApplet.P2D);
+	}
+
+	public void draw() {
+		mainGraphicsCtx.beginDraw();
+		mainGraphicsCtx.background(0);
+		mainGraphicsCtx.endDraw();
+		image(mainGraphicsCtx, MAIN_CTX_POS.x, MAIN_CTX_POS.y);
 	}
 
 	public static void main(String[] args) {
-		System.out.println(new Runner().getGreeting());
+		PApplet.main(new String[] { "wolfdungeon3d.Runner" });
 	}
 }
